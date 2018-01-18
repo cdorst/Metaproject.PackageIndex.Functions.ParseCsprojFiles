89d90c6af9d9a3176fd16a6a40538318f6f72a72
Christopher Dorst <chris@dorst.email>
2018-01-17 19:56:59 -0600

    Add CsprojFilesParser

---
+++ Metaproject.PackageIndex.Functions.ParseCsprojFiles/CsprojFilesParser.cs
@@ -0,0 +1,17 @@
+﻿using Metaproject.PackageIndex.Structures.PackageProject;
+using System.Collections.Generic;
+using System.IO;
+using static Metaproject.PackageIndex.Functions.ParseCsprojFile.CsprojFileParser;
+using static System.IO.Directory;
+
+namespace Metaproject.PackageIndex.Functions.ParseCsprojFiles
+{
+    public static class CsprojFilesParser
+    {
+        public static IEnumerable<PackageCsproj> ParseCsprojs(string directory)
+        {
+            foreach (var file in EnumerateFiles(directory, "*.csproj", SearchOption.AllDirectories))
+                yield return ParseCsproj(file);
+        }
+    }
+}
